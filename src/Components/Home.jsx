fe85a0ef3f7140dabd88a1321d536efa22e74662
naswih_mohd <naswihkkv786@gmail.com>
2024-12-12 15:23:54 +0530

    first committ

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../firebase/config'
-import { signOut } from 'firebase/auth'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { Outlet, Link } from 'react-router-dom'
 
 function Home() {
 
     const navigate = useNavigate()
+    const [user, setUser] = useState(false)
 
-    const handleLogout = ()=>{
-        signOut(auth).then(()=>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (User) => {
+            if (User) {
+                setUser(true);
+            } else {
+                setUser(false);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    console.log(user)
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
             console.log('success')
             navigate('/login')
         })
@@ -18,9 +33,15 @@ function Home() {
         <div className='flex items-center justify-center h-screen'>
             <div>
                 <h1 className='pb-3 font-bold text-2xl text-center'>7025133315</h1>
-                <button onClick={handleLogout} className='btn btn-primary w-96'>Logout</button>
+                {user ? (
+                    <button onClick={handleLogout} className='btn btn-primary w-96'>Logout</button>
+                ) : (
+                    <Link to={'/login'} ><button className='btn btn-success w-96'>Login</button></Link>
+                )}
+
                 <hr className='w-96 mt-5 ' />
             </div>
+            <Outlet />
         </div>
     )
 }
